0db73e6478dea4cc8bc0eaa6400d9a50f1ecd74b
mauriactis <37371033+mauriactis@users.noreply.github.com>
2018-03-15 11:44:40 +0100

    primo upload

---
+++ webApp/Homepage/clientlogic.js
@@ -0,0 +1,247 @@
+
+
+
+
+/*-------------------Funzioni in comune-------------------*/
+
+
+
+
+$(document).ready(function(){
+	caricaAnagrafica();
+});
+
+function cancellaCampi(){
+	 $("#txtNomePopupAggiungiNuovo").val("");
+	 $("#txtCognomePopupAggiungiNuovo").val("");
+	 $("#txtDataNascitaPopupAggiungiNuovo").val("");
+	 $("#txtIndirizzoPopupAggiungiNuovo").val("");
+	 $("#txtResidenzaPopupAggiungiNuovo").val("");
+	 $("#txtCapPopupAggiungiNuovo").val("");
+	 $("#txtLuogoNascitaPopupAggiungiNuovo").val("");
+	 $("#txtTelefonoPopupAggiungiNuovo").val("");
+	 $("#txtProvenienzaPopupAggiungiNuovo").val("");
+	 $("#txtMotivoPopupAggiungiNuovo").val("");
+	 $("#txtTelefono2PopupAggiungiNuovo").val("");
+	 $("#txtContattiPopupAggiungiNuovo").val("");
+	 $("#txtCodiceFiscalePopupAggiungiNuovo").val("");
+	 $("#txtOsservazioniPopupAggiungiNuovo").val("");
+
+	 if($("#campiAggiuntiviPopupAggiungiNuovo").is(":visible")){
+	 	changeArrow();
+	 }
+}
+
+function changeArrow(){
+	if(document.getElementById("btnAggiungiCampiPopupAggiungiNuovo").value == "Aggiungi"){
+		$("#campiAggiuntiviPopupAggiungiNuovo").toggle('show');
+        $("#btnAggiungiCampiPopupAggiungiNuovo").html("Nascondi campi <span class=\"glyphicon glyphicon-chevron-up\">");
+        document.getElementById("btnAggiungiCampiPopupAggiungiNuovo").value = "Nascondi";
+    }else{
+    	$("#campiAggiuntiviPopupAggiungiNuovo").toggle('hide');
+    	document.getElementById("btnAggiungiCampiPopupAggiungiNuovo").value = "Aggiungi";
+        $("#btnAggiungiCampiPopupAggiungiNuovo").html("Aggiungi campi <span class=\"glyphicon glyphicon-chevron-down\">");
+   }
+}
+
+function redimContainer(){
+	document.getElementById("container").style.marginRight = "0";
+}
+
+
+
+
+/*-------------------Funzioni relative ad anagrafica-------------------*/
+
+
+
+
+function caricaAnagrafica() {
+	if(document.getElementById("pagamento") != null){
+	 	nascondiPagamento();
+	}
+	if(document.getElementById("situazionePaziente") != null){
+	 	nascondiSituazionePaziente();
+	}
+	$.ajax({  
+        type: "GET", 
+        url: "../Anagrafica/index.html", 
+        success: function(response) {
+			var i;
+			$("#container").html(response);          
+        },
+        error: function(){
+        	alert("Errore");
+        }
+    });
+	$.ajax({  
+        type: "POST", 
+        url: "./serverlogic.php",
+        data: {azione: "caricaAnagrafica"},
+        success: function(response) {
+        	var persone = JSON.parse (response);
+			var riga = "";
+			for (var a = 0; a < persone.length; a ++)
+			{
+				riga += "<tr onclick=\"mostraSituazionePaziente(" + persone [a].ID + ");\"><td>" + persone [a].ID + "</td><td>" + persone [a].Nome + "</td><td>" + persone [a].Cognome + "</td><td>" + persone [a].DataNascita + "</td><td>" + persone [a].LuogoNascita + "</td><td>" + persone [a].Provenienza + "</td><td>" + persone [a].Residenza + "</td><td>" + persone [a].Indirizzo + "</td><td>" + persone [a].CAP + "</td><td>" + persone [a].Telefono1 + "</td><td>" + persone [a].Contatti + "</td><td>" + persone [a].Motivo + "</td><td>" + persone [a].CodFisc + "</td><td><button class=\"btn btn-danger\" onclick=\"visualizzaDocumenti(" + persone [a].id + ");\"><span class=\"glyphicon glyphicon-th-list\"></span></button></td><td>" + '<button class="btn btn-danger" onclick="visualizzaDocumenti(' + persone [a].id + ');"><span class="glyphicon glyphicon-th-list"></span></button>' + "</td><td>" + '<button class="btn btn-danger" onclick="generaCodice(' + persone [a].id + ');"><span class="glyphicon glyphicon-qrcode"></span></button>' + "</td></tr>";
+			}
+			$("#tblAnagraficaBody").html(riga);
+        },
+        error: function(){
+        	alert("Errore");
+        }
+    });
+}
+
+function cercaPersona ()
+{
+	var ricerca = document.getElementById ("txtRicercaAnagrafica").value;
+
+	$.post ("serverlogic.php?command=cercaPersona",
+		{
+			"persona": ricerca
+		},
+		function (data)
+		{
+			var persone = JSON.parse (data);
+			var t = $('#tblAnagrafica').DataTable ();
+			t.clear ();
+			
+
+			for (var a = 0; a < persone.length; a ++)
+			{
+				var d = [persone [a].ID,persone [a].nome, persone [a].cognome,persone [a].DataNascita,persone [a].LuogoNascita,persone [a].Provenienza,persone [a].Residenza,persone [a].Indirizzo,persone [a].CAP,persone [a].Telefono1,persone [a].Contatti,persone [a].Motivo,persone [a].Osservazioni,persone [a].CodiceFiscale];
+				t.row.add (d);
+			}
+			t.draw (false);
+
+		}
+	);
+
+	$.ajax({  
+        type: "POST", 
+        url: "./serverlogic.php",
+        data: {persona: ricerca,azione: "cercaPersona"},
+        success: function(response) {
+        	var persone = JSON.parse (response);
+			var t = $('#tblAnagrafica').DataTable ();
+			t.clear ();
+
+			for (var a = 0; a < persone.length; a ++)
+			{
+				var d = [persone [a].ID,persone [a].Nome, persone [a].Cognome,persone [a].DataNascita,persone [a].LuogoNascita,persone [a].Provenienza,persone [a].Residenza,persone [a].Indirizzo,persone [a].CAP,persone [a].Telefono1,persone [a].Contatti,persone [a].Motivo,persone [a].Osservazioni,persone [a].CodiceFiscale];
+				t.row.add (d);
+			}
+			t.draw (false);
+
+
+
+			//var riga="<table class=\"table table-hover table-bordered\" id=\"tblAnagrafica\"><thead id=\"tblIntestazione\"><tr><th>Nome</th><th>Cognome</th><th>Data di nascita</th><th>Luogo di nascita</th><th>Provenienza</th><th>Residenza</th><th>Indirizzo</th><th>CAP</th><th>Telefono</th><th>Contatti</th><th>Motivo</th><th>Osservazioni</th><th>Codice fiscale</th><th>Documenti</th></tr></thead><tbody id=\"bodyTblAnagrafica\">";
+			/*for(i=0;i<10;i++){ 
+				//Indice formato da ID
+				riga += "<tr onclick=\"mostraSituazionePaziente(" + i + ");\"> <td>Esempio</td> <td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td>Esempio</td><td><button type=\"button\" class=\"btn btn-danger\" id=\"btnDocumentiPaziente\"><span class=\"glyphicon glyphicon-list\"></span></button></td></tr>";
+			}*/
+			//riga += "</tbody></table>";
+			$("#container").html(riga + response);         
+        },
+        error: function(){
+        	alert("Errore");
+        }
+    });
+}
+
+function visualizzaDocumenti(id) {
+	
+
+
+
+
+
+}
+
+function generaCodice(id) {
+	
+
+
+
+
+
+}
+
+function mostraSituazionePaziente(i) {
+	if(document.getElementById("situazionePaziente").style.width == "500px"){
+	 	nascondiSituazionePaziente();
+	}else{
+		console.log(i);
+    	document.getElementById("situazionePaziente").style.width = "500px";
+    	document.getElementById("situazionePaziente").style.marginTop = "55px";
+    	document.getElementById("container").style.marginRight = "500px";
+	}
+}
+
+function nascondiSituazionePaziente() {
+	$("#txtSituazionePazienteOggi").val("");
+	$("#txtImportoSituazionePaziente").val("");
+	document.getElementById ("chkPagato").checked = false;
+    document.getElementById("situazionePaziente").style.width = "0";
+    redimContainer();
+}
+
+function checkButton(){
+	if(document.getElementById ("chkPagato").checked == true){
+		document.getElementById ("chkPagato").checked = false;
+	}else{
+		document.getElementById ("chkPagato").checked = true;
+	}
+}
+
+
+
+
+/*-------------------Funzioni relative a contabilità-------------------*/
+
+
+
+
+function caricaContabilita(){
+	if(document.getElementById("situazionePaziente") != null){
+	 	nascondiSituazionePaziente();
+	}
+	if(document.getElementById("pagamento") != null){
+	 	nascondiPagamento();
+	}
+	$.ajax({  
+        type: "GET", 
+        url: "../Contabilita/index.html", 
+        success: function(response) {
+			var i;
+			var riga="<table class=\"table table-hover table-bordered\" id=\"tblContabilita\"><thead id=\"tblIntestazione\"><tr><th>Nome</th><th>Cognome</th><th>Data</th><th>Importo</th></tr></thead><tbody id=\"bodyTblContabilita\">";
+			for(i=0;i<10;i++){ 
+				//Indice formato da ID + data
+				riga += "<tr onclick=\"mostraPagamento(" + i + ");\"> <td>Esempio</td> <td>Esempio</td><td>Esempio</td><td>50 &euro;</td></tr>";
+			}
+			riga += "</tbody></table>";
+			$("#container").html(riga + response);          
+        },
+        error: function(){
+        	alert("Errore");
+        }
+    });
+}
+
+function mostraPagamento(i) {
+	if(document.getElementById("pagamento").style.width == "500px"){
+	 	nascondiPagamento();
+	}else{
+		console.log(i);
+    	document.getElementById("pagamento").style.width = "500px";
+    	document.getElementById("pagamento").style.marginTop = "55px";
+    	document.getElementById("container").style.marginRight = "500px";
+	}
+}
+
+function nascondiPagamento() {
+	$("#txtImportoPagamento").val("");
+    document.getElementById("pagamento").style.width = "0";
+    redimContainer();
+}
---
+++ webApp/Homepage/serverlogic.php
@@ -0,0 +1,224 @@
+<?php
+	 
+	$user = "root"; $pwd = ""; $risposta = ""; $conn = null;
+		try{
+			// connessione
+			$conn = new PDO("mysql:host=localhost;dbname=provadb", $user, $pwd);
+			// abilita gestione errori tramite try … catch  (Exception)
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $ex){
+		   $risposta = "Errore connessione: ".$ex->getMessage();
+	}
+	
+	if(isset($_POST['azione']) && !empty($_POST['azione'])) {   //variabili tutte con minuscole con upper su prima lettere della seconda parola
+			$azione = $_POST['azione'];
+			switch($azione) {
+				case 'cercaPersona' : 
+					$persona = $_POST['nomePersona'];
+					cercaPersona($conn,$persona);
+					break;
+				case 'cercaContabilita' :
+					$persona = $_POST['nomePersona'];
+					cercaContabilita($conn,$persona);
+					break;
+				case 'caricaAnagrafica' :
+					caricaAnagrafica($conn);
+					break;	
+				case 'caricaUltimoIntervento' :
+					$idPersona = $_POST['id'];
+					caricaUltimoIntervento($conn,$id);
+					break;
+				case 'inserisciPagamentoDesc' :
+					$idPersona = $_POST['id'];
+					$importo = $_POST['importo'];
+					$pagato = $_POST['pagato'];
+					$descrizione = $_POST['descrizione'];
+					inserisciPagamentoDesc($conn,$idPersona,$importo,$pagato,$descrizione,$data);
+					break;
+				case 'inserisciNuovoPaziente' :
+					$idPersona = $_POST['id'];
+					$nome = $_POST['nome'];
+					$cognome = $_POST['cognome'];
+					$dataNascita = $_POST['dataNascita'];
+					$luogoNascita = $_POST['luogoNascita'];
+					$medicoProv = $_POST['medicoProv'];
+					$residenza = $_POST['residenza'];
+					$indirizzo = $_POST['indirizzo'];
+					$cap = $_POST['cap'];
+					$telefono1 = $_POST['telefono1'];
+					$telefono2 = $_POST['pagato'];
+					$motivo = $_POST['motivo'];
+					$anamnesi = $_POST['anamnesi'];
+					$codFisc = $_POST['codFisc'];
+					inserisciNuovoPaziente($conn,$idPersona,$nome,$cognome,$datanascita,$luogoNascita,$medicoProv,$residenza,$indirizzo,$cap,$telefono1,$telefono2,$motivo,$anamnesi,$codFisc);
+					break;
+				case 'visualizzaStoricoInterventi' :
+					$idPersona = $_POST['id'];
+					visualizzaStoricoInterventi($conn,$idPersona);
+					break;
+				case 'visualizzaContabilitaPersona' :
+					$idPersona = $_POST['id'];
+					visualizzaContabilitaPersona($conn,$idPersona);
+					break;
+				}
+			}
+	
+		$conn=null;
+
+		function cercaPersona($conn,$persona){   //Il cerca nella scheda anagrafica
+
+			$persona = strtoupper($persona);
+			$persona = "%".$persona."%";
+			$query="select ID,Nome,Cognome,DataNascita,LuogoNascita,Provenienza,Residenza,Indirizzo,CAP,Telefono1,Contatti,Motivo,Osservazioni1,CodiceFiscale from anagrafica where upper(Nome) Like ? or upper(Cognome) Like ? order by Cognome,Nome";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $persona);
+			$stmSql ->bindParam(2, $persona);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret)); //tommy prendi questo e costruisci la tabella (ho modificato il js ed ho aggiunto una funzione che mi passava il nomePersona con ajax)
+
+		}
+
+
+		function cercaContabilita($conn,$persona){   //Il cerca nella scheda contabilita
+
+			$persona = strtoupper($persona);   
+			$persona = "%".$persona."%";             //ricerca se c e persona dentro alla stringa che scriviamo
+			$query="select AnaID,Nome,Cognome,Data,Pagamento,Pagato form anagrafica,pagamenti where anagrafica.ID = pagamenti.AnaID upper(Nome) Like ? or upper(Cognome) Like ? order by Cognome,Nome,Data";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $persona);
+			$stmSql ->bindParam(2, $persona);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret)); 
+
+		}
+
+		function caricaAnagrafica($conn){   //funzione che viene richiamata appena viene startata la pagina
+			
+			$query="select ID,Nome,Cognome,DataNascita,LuogoNascita,MedicoProvenienza,Residenza,Indirizzo,CAP,Telefono1,Telefono2,Motivo,Anamnesi,CodFisc from anagrafica order by
+			Cognome,Nome";
+			$stmSql = $conn->prepare($query);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret));
+
+		}
+
+		function local_encode ($var){
+			if (is_string ($var))
+				return utf8_encode ($var);
+			if (is_array ($var))
+				return array_map ('local_encode', $var);
+			return $var;
+		}
+
+		function caricaUltimoIntervento($conn,$idPersona){   //carica l ultimo intervento da mettere nella casella a destra per far vedere cosa è stato fatto la volta precedente
+
+			$query="select Data,Descrizione from interventi where AnaID = ? order by Data DESC";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $idPersona);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret));
+
+		}
+
+
+		function inserisciPagamentoDesc($conn,$idPersona,$data,$importo,$pagato,$descrizione){   //inserisce il pagamento nel database dopo che la dott. ha finito e aggiunge il costo delle seduto con descrizione
+
+			$query="insert into pagamenti values(?,?,?,?,?)";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $idPersona);
+			$stmSql ->bindParam(2, $data);
+			$stmSql ->bindParam(3, $importo);
+			$stmSql ->bindParam(4, $descrizione);
+			$stmSql ->bindParam(5, $pagato);
+			
+			$result = $stmSql ->execute();
+			
+		echo $result;          //faccio restituire solo vero o falso se riesce eseguire la query da echo vero
+
+		}
+
+		function inserisciNuovoPaziente($conn,$idPersona,$nome,$cognome,$datanascita,$luogoNascita,$medicoProv,$residenza,$indirizzo,$cap,$telefono1,$telefono2,$motivo,$anamnesi,$codFisc){   //inserisce un nuovo utente nel db
+
+			$query="insert into anagrafica values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $idPersona);
+			$stmSql ->bindParam(2, $nome);
+			$stmSql ->bindParam(3, $cognome);
+			$stmSql ->bindParam(4, $datanascita);
+			$stmSql ->bindParam(5, $luogoNascita);
+			$stmSql ->bindParam(6, $medicoProv);
+			$stmSql ->bindParam(7, $residenza);
+			$stmSql ->bindParam(8, $indirizzo);
+			$stmSql ->bindParam(9, $cap);
+			$stmSql ->bindParam(10, $telefono1);
+			$stmSql ->bindParam(11, $telefono2);
+			$stmSql ->bindParam(12, $motivo);
+			$stmSql ->bindParam(13, $anamnesi);
+			$stmSql ->bindParam(14, $codFisc);
+			
+			$result = $stmSql ->execute();
+			
+		echo $result;			//faccio restituire solo vero o falso se riesce eseguire la query da echo vero
+
+		}
+
+		function visualizzaStoricoInterventi($conn,$idPersona){   //pulsante che chiede tutti gli ultimi interventi
+
+			$query="select AnaId,Data,Importo,Descrizione,Pagato from interventi where AnaID = ? order by data DESC";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $idPersona);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret));
+
+		}
+
+		function visualizzaContabilitaPersona($conn,$idPersona){  //restituisce i record riguardati la contabilita del paziente dal menu a scorrimento a destra
+
+			$query="select AnaId,Data,Importo,Descrizione,Pagato from interventi where AnaID = ? order by data DESC";
+			$stmSql = $conn->prepare($query);
+			$stmSql ->bindParam(1, $idPersona);
+			$result = $stmSql ->execute();
+			$ret= array();
+
+			while ($row = $stmSql->fetch()){
+					array_push ($ret, $row);
+			}
+			
+		echo json_encode(local_encode($ret));
+
+		}
+
+		
+
+?>
+
