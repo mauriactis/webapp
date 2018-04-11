7c855d7a68de608daa00a2667c1ba57f51e813a7
GabriTom <37402818+GabriTom@users.noreply.github.com>
2018-04-11 21:10:27 +0200

    Aggiornamenti
    css:
    Aggiunto il codice relativo al sidenav
    giorniAppuntamentiCentrato insieme alla tabella sotto e spostamento a sinistra del calendario
    html:
    aggiunti div per impaginazione
    tabella di esempio
    js:
    metodi per visualizzare e nascondere il sidenav
    salva dettagli appuntamento () da testare
    checkfields dettagli appuntamento
    svuota dettagli appuntamento

--- webApp/Appuntamenti/clientlogic.js
+++ webApp/Appuntamenti/clientlogic.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $("#vsblPage").dblclick(function() {
+        if(document.getElementById("sideDettagliAppuntamento").style.width == "500px"){
+            nascondiDettagliAppuntamento();
+        }
+    });
+});
+
 function initForm(){
     $.datepicker.setDefaults($.datepicker.regional['it']);
     $('#pckrDataAppuntamento').datepicker({minDate: new Date()});
@@ -33,4 +41,50 @@ function giraDataDb(date){
 function stampaData(){
     var data = document.getElementById ("pckrDataAppuntamento").value;
     console.log(data);
+}
+
+function mostraDettagliAppuntamento(){
+    document.getElementById("sideDettagliAppuntamento").style.width = "500px";
+    document.getElementById("sideDettagliAppuntamento").style.marginTop = "70px";
+}
+
+function nascondiDettagliAppuntamento(){
+    $("#txtAppuntiDettagliAppuntamento").val("");
+    svuotaAppunti();
+    document.getElementById("sideDettagliAppuntamento").style.width = "0";
+}
+
+function salvaDettagliAppuntamento(){
+    var appunti = $("#txtAppuntiDettagliAppuntamento");
+    if(checkfieldsDettagliAppuntamento(appunti)){
+        /*$.ajax({  
+            type: "POST", 
+            url: "./serverlogic.php",
+            data: {azione: "inserisciAppuntiAppuntamento", appunti:appunti},
+            success: function(response) {
+                alert("Memorizzazione avvenuta con successo!");
+            },
+            error: function(){
+                alert("Errore");
+            }
+        });*/
+    }else{
+        alert("Alcuni campi non sono stati completati correttamente...");
+    }
+}
+
+//Restituisce vero se va tutto bene
+function checkfieldsDettagliAppuntamento(appunti){
+    var ret = true;
+
+    if(appunti.val() == ""){
+        appunti.css("background-color", "rgb(255,147,147)");
+        ret = false;
+    }
+
+    return ret;
+}
+
+function svuotaAppunti(){
+    document.getElementById("txtAppuntiDettagliAppuntamento").style.backgroundColor = "white";
 }
